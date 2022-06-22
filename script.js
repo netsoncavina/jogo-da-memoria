92d218a96c14c08fd64ec21c6b9ad4b000ce26df
Netson <netsoncavina@gmail.com>
2022-06-22 05:17:27 -0300

    adicionadas funções para finalizar o jogo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const cards = document.querySelectorAll(".card");
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
+let matches = 0;
 
 function flipCard() {
   if (lockBoard) return;
@@ -17,11 +18,65 @@ function flipCard() {
   secondCard = this;
   hasFlippedCard = false;
   checkForMatch();
+  checkWin();
+  reduceMovements();
+  checkLose();
 }
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.card === secondCard.dataset.card;
-  isMatch ? disableCards() : unflipCards();
+  isMatch ? (disableCards(), matches++) : unflipCards();
+}
+
+function reduceMovements() {
+  let movements = document.querySelector(".movimentos");
+  let counter = movements.innerHTML;
+  movements.innerHTML >= 0 ? (movements.innerHTML = counter - 1) : null;
+}
+
+function checkWin() {
+  matches == 6 ? showAlert("win") : null;
+}
+
+function checkLose() {
+  let movements = document.querySelector(".movimentos").innerHTML;
+
+  movements == -1 && matches != 6 ? showAlert("lose") : null;
+}
+
+function showAlert(condition) {
+  const alert = Swal.mixin({
+    position: "center",
+    showConfirmButton: false,
+    timer: 4000,
+    timerProgressBar: true,
+    didOpen: (alert) => {
+      alert.addEventListener("mouseenter", Swal.stopTimer);
+      alert.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  condition == "win"
+    ? alert
+        .fire({
+          icon: "success",
+          title: "Você ganhou!",
+        })
+        .then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            document.location.reload(true);
+          }
+        })
+    : alert
+        .fire({
+          icon: "error",
+          title: "Sem mais movimentos!",
+        })
+        .then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            document.location.reload(true);
+          }
+        });
 }
 
 function disableCards() {
